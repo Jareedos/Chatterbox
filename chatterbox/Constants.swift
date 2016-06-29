cbfde05d369bfe091b16bf550f082080b1f79caa
Jareedos <jaredallens@gmail.com>
2016-06-29 13:55:18 -0700

    Created Constants file in Util folder & Created a constant color for shadowing my views

---
+++ chatterbox/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  chatterbox
+//
+//  Created by Jared Sobol on 6/29/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ chatterbox/MaterialView.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialView.swift
+//  chatterbox
+//
+//  Created by Jared Sobol on 6/29/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+//        layer.shadowColor
+    }
+}
