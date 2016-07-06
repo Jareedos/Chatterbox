2ce777adfa63c710aa71a7cda0599299123461eb
Jareedos <jaredallens@gmail.com>
2016-07-06 13:06:59 -0700

    Intergrated ShowErrorAlert Func for custumizable error messges

--- chatterbox/ViewController.swift
+++ chatterbox/ViewController.swift
@@ -15,14 +15,20 @@ import Firebase
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) != nil {
+            self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+        }
     }
     
     @IBAction func fbBtnPressed(sender: UIButton!) {
@@ -61,7 +67,22 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func attemptLogin(sender: UIButton!) {
+        if let email = emailField.text where email != "", let pwd = passwordField.text where pwd != "" {
+            
+        } else {
+            showErrorAlert("Email and Password Required", msg: "Your must enter an Email and Password")
+        }
+        
+    }
     
+    func showErrorAlert(title: String, msg: String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default , handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+        
+    }
     
     
 }
