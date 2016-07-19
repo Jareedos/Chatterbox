37b6033b722759b197009e989d2a46afd0bf523e
Jareedos <jaredallens@gmail.com>
2016-07-19 11:44:40 -0700

    New FeedVC file for TableView. Intergrated TableView and required functions

---
+++ chatterbox/FeedVC.swift
@@ -0,0 +1,33 @@
+//
+//  FeedVC.swift
+//  chatterbox
+//
+//  Created by Jared Sobol on 7/19/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
